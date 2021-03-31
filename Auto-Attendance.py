7e5e981df12d7b24b8a1fd67c39591af4df7cb6b
aniket <dummyani26@gmail.com>
2021-03-31 09:36:50 +0530

    Check that student has written his roll number
    Keep only previous 10 sheets in folder, delete rest.
    Check that Zoom chat folder and file exist.

--- Auto-Attendance.py
+++ Auto-Attendance.py
@@ -16,6 +16,67 @@ DAYS_PRESENT_COLUMN = "C"
 
 BACKUP_DAYS = 10
 
+NAMES = [
+    "Aarish Khanna",
+    "Aarushi",
+    "Abheer Narula",
+    "Abhinav Taya",
+    "Abhishek Kumar Mishra",
+    "Aditya Pangtey",
+    "Akhil Bhatt",
+    "Aman Kapoor",
+    "Aman Kumar Sah",
+    "Aman Sharma",
+    "Aman Vashist",
+    "Amol Jandial",
+    "Andrew Awet Thon Ghum",
+    "Aniket Banyal",
+    "Aniket Kumar Sinha",
+    "Ankush Gupta",
+    "Anmol Reshi",
+    "Anshuman Sharma",
+    "Anuj Pal",
+    "Anukool Chandra",
+    "Arshjot Singh",
+    "Aryaman Malik",
+    "Aryan Chauhan",
+    "Ayush Sahnu",
+    "Bhavya Nayyer",
+    "Bhawna",
+    "Bhuvan Dhir",
+    "Chahat Kalsi",
+    "Chhavi Dua",
+    "Deepak Hutthy",
+    "Dhriti",
+    "Divyam Gupta",
+    "Ekjot Singh",
+    "Garvit Chhabra",
+    "Gaurav Choudhary",
+    "Gur Rehmat Singh Chahal",
+    "Gursewak Singh",
+    "Harsh Singh",
+    "Himesh Yadav",
+    "Hitanshu Goyal",
+    "Ishaan Bhardwaj",
+    "Japnoor Monga",
+    "Jashanjot Kaur",
+    "Jogeshwar Baporia",
+    "Jogeshwar Singh",
+    "Jotbir Singh",
+    "Kamaljeet Kaur",
+    "Kanav Jain",
+    "Kartik Khandelwal",
+    "Kartik Malik",
+    "Kartik Sharma",
+    "Kartikay Mahajan",
+    "Kashvi Garg",
+    "Keshav Bindal",
+    "Krishna Sharma",
+    "Kushagra Sharma",
+    "Lakshitaa Sehgal",
+    "Lavish Lamba",
+]
+
 
 def generateRollNumberList():
     roll_number_list = []
@@ -28,70 +89,10 @@ def generateRollNumberList():
     return roll_number_list
 
 
+ROLL_NUMBER_LIST = generateRollNumberList()
+
+
 def createInitialSheet(file_name):
-    roll_number_list = generateRollNumberList()
-
-    names = [
-        "Aarish Khanna",
-        "Aarushi",
-        "Abheer Narula",
-        "Abhinav Taya",
-        "Abhishek Kumar Mishra",
-        "Aditya Pangtey",
-        "Akhil Bhatt",
-        "Aman Kapoor",
-        "Aman Kumar Sah",
-        "Aman Sharma",
-        "Aman Vashist",
-        "Amol Jandial",
-        "Andrew Awet Thon Ghum",
-        "Aniket Banyal",
-        "Aniket Kumar Sinha",
-        "Ankush Gupta",
-        "Anmol Reshi",
-        "Anshuman Sharma",
-        "Anuj Pal",
-        "Anukool Chandra",
-        "Arshjot Singh",
-        "Aryaman Malik",
-        "Aryan Chauhan",
-        "Ayush Sahnu",
-        "Bhavya Nayyer",
-        "Bhawna",
-        "Bhuvan Dhir",
-        "Chahat Kalsi",
-        "Chhavi Dua",
-        "Deepak Hutthy",
-        "Dhriti",
-        "Divyam Gupta",
-        "Ekjot Singh",
-        "Garvit Chhabra",
-        "Gaurav Choudhary",
-        "Gur Rehmat Singh Chahal",
-        "Gursewak Singh",
-        "Harsh Singh",
-        "Himesh Yadav",
-        "Hitanshu Goyal",
-        "Ishaan Bhardwaj",
-        "Japnoor Monga",
-        "Jashanjot Kaur",
-        "Jogeshwar Baporia",
-        "Jogeshwar Singh",
-        "Jotbir Singh",
-        "Kamaljeet Kaur",
-        "Kanav Jain",
-        "Kartik Khandelwal",
-        "Kartik Malik",
-        "Kartik Sharma",
-        "Kartikay Mahajan",
-        "Kashvi Garg",
-        "Keshav Bindal",
-        "Krishna Sharma",
-        "Kushagra Sharma",
-        "Lakshitaa Sehgal",
-        "Lavish Lamba",
-    ]
-
     workbook = Workbook()
     sheet = workbook.active
 
@@ -99,10 +100,10 @@ def createInitialSheet(file_name):
     sheet[f"{NAME_COLUMN}1"] = "Name"
     sheet[f"{DAYS_PRESENT_COLUMN}1"] = "Days Present"
 
-    for i in range(len(roll_number_list)):
+    for i in range(len(ROLL_NUMBER_LIST)):
         row = i + 2
-        sheet[f"{ROLL_NUMBER_COLUMN}{row}"] = roll_number_list[i]
-        sheet[f"{NAME_COLUMN}{row}"] = names[i]
+        sheet[f"{ROLL_NUMBER_COLUMN}{row}"] = ROLL_NUMBER_LIST[i]
+        sheet[f"{NAME_COLUMN}{row}"] = NAMES[i]
         sheet[f"{DAYS_PRESENT_COLUMN}{row}"] = 0
 
     workbook.save(filename=file_name)
@@ -117,19 +118,32 @@ def printEndMesaage(roll_nos, date):
     print(f"{len(roll_nos)} students present on {date}\n")
 
 
+def customSort(file):
+    file_date = file.split(".")[0]
+    return datetime.strptime(file_date, "%Y-%m-%d").date()
+
+
 def cleanup():
+    files = getAllSheetsInSortedOrder()
+
+    if len(files) > BACKUP_DAYS:
+        for i in range(len(files) - BACKUP_DAYS):
+            os.remove(os.path.join(XL_FILE_BASE_PATH, files[i]))
+
+
+def getAllSheetsInSortedOrder():
+    files = []
     for file in os.listdir(XL_FILE_BASE_PATH):
         if file.startswith("~$"):
             continue
-
-        if file == "temp.xlsx":
+        elif file == "temp.xlsx":
             os.remove(os.path.join(XL_FILE_BASE_PATH, file))
             continue
+        files.append(file)
 
-        file_date = file.split(".")[0]
-        file_date = datetime.strptime(file_date, "%Y-%m-%d").date()
-        if file_date < todays_date - timedelta(days=BACKUP_DAYS):
-            os.remove(os.path.join(XL_FILE_BASE_PATH, file))
+    files.sort(key=customSort)
+
+    return files
 
 
 def getDaysPresent(new_column_int, row, sheet):
@@ -143,16 +157,35 @@ def getDaysPresent(new_column_int, row, sheet):
     return days_present
 
 
+def getNameFromRollNumber(roll_no):
+    idx = ROLL_NUMBER_LIST.index(roll_no)
+    return NAMES[idx]
+
+
 def getPresentRollNumbersFromFile(file_path):
     roll_nos = []
 
-    with open(file_path) as f:
+    with open(file_path, encoding="utf-8") as f:
         for line in f:
-            for word in line.split():
-                if "ue193" in word.lower():
+            words = line.split()
+
+            for word in words:
+                if word == "From":
+                    user_name = words[words.index(word) + 1]
+
+                elif "ue193" in word.lower():
                     roll_no = "UE" + re.findall("\d+", word)[0]
-                    if roll_no not in roll_nos:
-                        roll_nos.append(roll_no)
+                    name = getNameFromRollNumber(roll_no)
+
+                    if name.split()[0].lower() == user_name.lower():
+                        if roll_no not in roll_nos:
+                            roll_nos.append(roll_no)
+                    else:
+                        print(
+                            f"Problem encountered - Zoom user name: {user_name}, Roll Number: {roll_no}, Real name: {name}"
+                        )
+                    break
+
     return roll_nos
 
 
@@ -179,7 +212,6 @@ def saveNewSheet(previous_sheet, txt_file, date):
         file_name = os.path.join(XL_FILE_BASE_PATH, f"{TODAYS_DATE}.xlsx")
         workbook.save(filename=file_name)
 
-        cleanup()
         printEndMesaage(present_roll_nos, date)
 
     except PermissionError:
@@ -187,14 +219,27 @@ def saveNewSheet(previous_sheet, txt_file, date):
 
 
 def run(previous_sheet):
+    todays_chat_folder_exists = False
+
     for folder in os.listdir(TXT_FILE_BASE_PATH):
         date = folder.split()[0]
         if date == TODAYS_DATE:
             if "ada" in folder.lower():
+                todays_chat_folder_exists = True
                 folder_path = os.path.join(TXT_FILE_BASE_PATH, folder)
-                txt_file = os.path.join(folder_path, os.listdir(folder_path)[0])
-                saveNewSheet(previous_sheet, txt_file, date)
+                try:
+                    txt_file = os.path.join(folder_path, os.listdir(folder_path)[0])
+                    saveNewSheet(previous_sheet, txt_file, date)
+                except IndexError:
+                    print(f"\nZoom chat file for {TODAYS_DATE} doesn't exist.")
+                    print(f"Please check '{folder_path}' folder.\n")
                 break
+
+    if not todays_chat_folder_exists:
+        print(f"\nZoom chat folder for {TODAYS_DATE} doesn't exist.")
+        print(f"Please check '{TXT_FILE_BASE_PATH}' folder.\n")
+
+    cleanup()
 
 
 def getPreviousSheet():
@@ -219,3 +264,4 @@ def getPreviousSheet():
 
 if __name__ == "__main__":
     run(getPreviousSheet())
+    input()
